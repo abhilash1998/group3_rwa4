ea1cd3958a347e586edf14352d98bc676ede54e6
R. Nick Vandemark <rnvandemark@gmail.com>
2022-02-26 14:37:51 -0500

    Fix ROS connection persistence

--- src/agvs_controller.cpp
+++ src/agvs_controller.cpp
@@ -1,5 +1,8 @@
 #include "ariac_agv.hpp"
+
 #include <ros/ros.h>
+
+#include <memory>
 
 #define NUM_AGVS 4
 
@@ -7,12 +10,12 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "agvs_controller");
     ros::NodeHandle nh;
-    std::vector<AriacAgv> agvs;
+    std::vector<std::unique_ptr<AriacAgv>> agvs;
     for (int i = 0; i < NUM_AGVS; i++)
     {
         // AGV topics use identifiers in the range [1,NUM_AGVS], but this loop
         // is [0,NUM_AGVS-1], so add 1 to all indices when creating them here
-        agvs.push_back(AriacAgv(&nh, i+1));
+        agvs.push_back(std::unique_ptr<AriacAgv>(new AriacAgv(&nh, i+1)));
     }
     ros::spin();
     return 0;
