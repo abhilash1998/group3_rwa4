b892fde228c1a3813733acb2b7308ae36634ec31
Sameep Pote <sameepote2808@gmail.com>
2022-02-26 22:23:53 -0500

    Rectifying sensor_sub

--- include/sub_sensor.hpp
+++ include/sub_sensor.hpp
@@ -13,11 +13,31 @@
 #include <std_msgs/String.h>
 #include <std_srvs/Trigger.h>
 #include <trajectory_msgs/JointTrajectory.h>
+
+namespace ros {
+    class NodeHandle;
+}
+
 class Sensors {
+
+protected:
+	ros::Subscriber breakbeam0_sub;
+	ros::Subscriber breakbeam0_change_sub;
+	ros::Subscriber logical_camera_bins0_sub;
+	ros::Subscriber logical_camera_station2_sub;
+	ros::Subscriber proximity_sensor_0_sub;
+	ros::Subscriber laser_profiler_0_sub;
+	ros::Subscriber quality_control_sensor_1_sub;
+	ros::Subscriber quality_control_sensor_2_sub;
+	ros::Subscriber quality_control_sensor_3_sub;
+	ros::Subscriber quality_control_sensor_4_sub;
+
 public:
-explicit Sensors(ros::NodeHandle);
+explicit Sensors(ros::NodeHandle* const nh);
 
 void break_beam_callback(const nist_gear::Proximity::ConstPtr& msg);
+
+void break_beam_change_callback(const nist_gear::Proximity::ConstPtr& msg);
 
 void laser_profiler_callback(const sensor_msgs::LaserScan::ConstPtr& msg);
 
@@ -44,7 +64,6 @@ void logical_camera_callback2(
 void startdetect();
 
 private:
-
-ros::NodeHandle n;
+ros::NodeHandle* n;
 
 };
