6be72e73b19cd6f47d3bc8bb56516e8cb4885a11
R. Nick Vandemark <rnvandemark@gmail.com>
2022-04-23 13:59:28 -0400

    Begin combining all functionality into single node

--- src/arm_controller.cpp
+++ src/arm_controller.cpp
@@ -3,19 +3,105 @@
 #include <std_msgs/String.h>
 #include <std_srvs/Trigger.h>
 
+#include <memory>
+#include <unordered_map>
+
+#include "agility.hpp"
+#include "ariac_agv.hpp"
 #include "arm.hpp"
+
+#define NUM_AGVS 4
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "arm_controller");
     ros::NodeHandle nh;
 
+    // Create interfaces to each of the AGVs
+    std::unordered_map<std::string, std::shared_ptr<AriacAgv>> agv_map;
+    for (int i = 0; i < NUM_AGVS; i++)
+    {
+        // AGV topics use identifiers in the range [1,NUM_AGVS], but this loop
+        // is [0,NUM_AGVS-1], so add 1 to all indices when creating them here
+        auto agv = std::shared_ptr<AriacAgv>(new AriacAgv(&nh, i+1));
+        agv_map[agv->get_id()] = agv;
+    }
+
     ros::AsyncSpinner spinner(0);
     spinner.start();
 
+    AgilityChallenger agility(&nh);
     Arm arm;
+
+    //
+    // Start the competition
+    //
+
+    ros::Duration wait_for_competition_state(0.1);
+    bool competition_state_valid = false;
+    std::string competition_state;
+    ros::Subscriber competition_state_sub = nh.subscribe<std_msgs::String>(
+        "/ariac/competition_state",
+        1,
+        [&](const std_msgs::String::ConstPtr& msg) 
+        {
+            competition_state = msg->data;
+            competition_state_valid = true;
+        }
+    );
+
+    // Spin the node until we've collected the competition state
+    while (!competition_state_valid)
+    {
+        wait_for_competition_state.sleep();
+        ros::spinOnce();
+    }
+
+    // If the competition has not started, we place a request to start it
+    if (competition_state == "go")
+    {
+        ROS_INFO_STREAM("Competition is already started.");
+    }
+    else
+    {
+        // Create the client
+        assert(competition_state == "init");
+        ros::ServiceClient start_competition_scl = nh.serviceClient<std_srvs::Trigger>("/ariac/start_competition");
+        // Wait a very long time, because this service server may take a while
+        // to be created
+        assert(start_competition_scl.waitForExistence(ros::Duration(30.0)));
+
+        // Place the request
+        std_srvs::Trigger start_competition_srv;
+        if (!(start_competition_scl.call(start_competition_srv) && start_competition_srv.response.success))
+        {
+            ROS_ERROR_STREAM("Failed to start the competition: '"<< start_competition_srv.response.message);
+            return 1;
+        }
+
+        // Wait a little and collect the updated competition state
+        competition_state_valid = false;
+        ros::Duration(0.25).sleep();
+        ros::spinOnce();
+        if (competition_state == "go")
+        {
+            ROS_INFO_STREAM("Competition was started.");
+        }
+        else
+        {
+            ROS_ERROR_STREAM("Competition state did not update as expected: '"<< competition_state);
+            return 2;
+        }
+    }
+
+    //
+    // If we're here, the competition has started
+    //
+
     arm.goToPresetLocation("home1");
     arm.goToPresetLocation("home2");
+
+    ros::waitForShutdown();
 
     return 0;
 }
